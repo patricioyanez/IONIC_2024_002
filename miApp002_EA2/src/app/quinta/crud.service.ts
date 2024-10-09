bb517eac7c3f8a8563f22050e5ddeedf8affd1f4
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-09 12:00:36 -0300

    update

---
+++ miApp002_EA2/src/app/quinta/crud.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudService {
+
+  constructor() { }
+
+  async guardar(id:string, valor: any)
+  {
+    
+  }
+  async leer(id: string)
+  {
+
+  }
+  async eliminar(id:string)
+  {
+
+  }
+  async listar()
+  {
+
+  }
+  async buscarPorRut(rut:string)
+  {
+
+  }
+}
