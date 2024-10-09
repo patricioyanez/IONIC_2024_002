e7bcd9cf46e012bbda68194f601a69bb12084f4b
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-09 11:16:10 -0300

    update

--- miApp002_EA2/src/app/cuarta/cuarta.page.ts
+++ miApp002_EA2/src/app/cuarta/cuarta.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController, ToastController } from '@ionic/angular';
+AlertController
 @Component({
   selector: 'app-cuarta',
   templateUrl: './cuarta.page.html',
@@ -8,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class CuartaPage implements OnInit {
   id:string = '';
   nombre:string = '';
-  constructor() { }
+  constructor(private alertController: AlertController,
+              private toastController: ToastController
+  ) { }
 /**
  * Ejercicio 19: Crear un formulario con text para
  * codigo y nombre y almecenar en el localstorage
@@ -18,10 +21,43 @@ export class CuartaPage implements OnInit {
   }
   limpiar()
   {
-
+    this.id = this.nombre = "";
   }
-  guardar()
+  async guardar()
   {
-
+    if(this.id == "" || this.id == "0")
+    {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        subHeader: 'Valor Id',
+        message: 'El valor indicado no es válido',
+        buttons: ['Aceptar']
+      });
+      await alert.present();
+    }
+    else if(this.nombre.trim().length < 1)
+    {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        subHeader: 'Nombre',
+        message: 'El nombre no es válido',
+        buttons: ['Aceptar']
+      });
+      await alert.present();
+    }
+    else
+    {
+      localStorage.setItem(this.id, this.nombre);
+      const toast = await this.toastController.create({
+        header: 'Información',
+        message: 'Los datos fueron guardados',
+        icon: 'checkmark-circle-outline',
+        duration: 4000,
+        color: 'success',
+        position: 'middle',
+        buttons: ['Aceptar']
+      });
+      await toast.present();
+    }
   }
 }
